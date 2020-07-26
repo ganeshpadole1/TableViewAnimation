67a8936aa35e135cfb9e75d5f32f213a113808b6
ganesh padole <ganeshpadole55@gmail.com>
2020-07-26 14:05:25 +0530

    Initial Commit

---
+++ TableViewAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewAnimation
+//
+//  Created by Ganesh Padole on 26/07/20.
+//  Copyright © 2020 Ganesh Padole. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
